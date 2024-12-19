87afc8593c50f4a1aa228ed904ad5fa227e8e08c
Roman Peka <pekaromanmail@gmail.com>
2024-12-19 03:05:47 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -8,16 +8,20 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
+    Person.people = {}
+
     for person in people:
         name, age = person["name"], person["age"]
         Person(name, age)
 
     for person in people:
         name = person["name"]
-        spouse = person.get("wife") or person.get("husband")
-        if spouse:
-            setattr(Person.people[name],
-                    "wife" if "wife" in person else "husband",
-                    Person.people[spouse])
+        if "wife" in person and person["wife"]:
+            Person.people[name].wife = Person.people[person["wife"]]
+            Person.people[person["wife"]].husband = Person.people[name]
+
+        if "husband" in person and person["husband"]:
+            Person.people[name].husband = Person.people[person["husband"]]
+            Person.people[person["husband"]].wife = Person.people[name]
 
     return list(Person.people.values())
